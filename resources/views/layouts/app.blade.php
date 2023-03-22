374858411e0b655abe6c39be65aaebd8d8d058be
E <evgheni.pirvan@profesor.cihcahul.md>
2023-03-22 13:11:08 +0200

    Header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,1 +1,34 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+</head>
+<body>
+    <header>
+        <nav class="main-nav">
+            <a href="">
+                <img src="{{ asset('imagini/logo.png') }}" class="logo">
+            </a>
+            <nav class="links">
+                <a href="#">Despre</a>
+                <a href="#">Produse metodice</a>
+                <a href="#">Exemple de lucrari</a>
+            </nav>
+        </nav>
+        <div class="home-content">
+            <h1>Platforma de promovare</h1>
+            <p>Centrul Republican de Învățământ din subordinea Ministerului Educației Publice Platforma unică servește la popularizarea celor mai bune practici ale pedagogilor</p>
+            <div class="buttons">
+                <a href="#" class="register">Inscrie-te</a>
+                <a href="#" class="login">Autentificare</a>
+            </div>
+        </div>
+    </header>
+
+</body>
+</html>
